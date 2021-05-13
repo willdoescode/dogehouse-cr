eebd27de113d983b4cde4a586f96f9c6b4c900c9
Will Lane <williamlane923@gmail.com>
2021-05-12 23:36:37 -0700

    added ability to join room

--- client.cr
+++ client.cr
@@ -6,6 +6,23 @@ TIMEOUT = 8
 
 ws = HTTP::WebSocket.new(URI.parse(API_URL))
 
+def join_room(socket : HTTP::WebSocket, room : String)
+  join = JSON.build do |json|
+    json.object do
+      json.field "op", "room:join"
+      json.field "d" do
+        json.object do
+          json.field "roomId", room
+        end
+      end
+      json.field "ref", "[uuid]"
+      json.field "v", "0.2.0"
+    end
+  end
+
+  socket.send join
+end
+
 ws.on_message do |msg|
   puts msg
 end
@@ -30,7 +47,7 @@ auth = JSON.build do |json|
         json.field "accessToken", ENV["ACCESS_TOKEN"]
         json.field "refreshToken", ENV["REFRESH_TOKEN"]
         json.field "reconnectToVoice", false
-        json.field "currentRoomId", ""
+        json.field "currentRoomId", ENV["ROOM_ID"]
         json.field "muted", true
         json.field "platform", "dogehouse-cr"
       end
@@ -39,5 +56,7 @@ auth = JSON.build do |json|
 end
 
 ws.send auth
+
+join_room ws, "c1089644-cc15-4204-8935-336b5a7fa83c"
 
 ws.run
