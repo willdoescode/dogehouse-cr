ff2c800bb7ea8ad288f871461f666623f5dc10fe
Will Lane <williamlane923@gmail.com>
2021-05-12 23:50:14 -0700

    switched from JSON.build to hashmap.to_json

--- client.cr
+++ client.cr
@@ -7,25 +7,41 @@ TIMEOUT = 8
 ws = HTTP::WebSocket.new(URI.parse(API_URL))
 
 def join_room(socket : HTTP::WebSocket, room : String)
-  join = JSON.build do |json|
-    json.object do
-      json.field "op", "room:join"
-      json.field "d" do
-        json.object do
-          json.field "roomId", room
-        end
-      end
-      json.field "ref", "[uuid]"
-      json.field "v", "0.2.0"
-    end
-  end
+  socket.send(
+    {
+      "op" => "room:join",
+       "d" => {
+         "roomId" => room
+       },
+       "ref" => "[uuid]",
+       "v" => "0.2.0"
+    }.to_json
+  )
+end
 
-  socket.send join
+def auth(socket : HTTP::WebSocket, token : String, refreshToken : String)
+  socket.send(
+    {
+      "op" => "auth", 
+      "d" => {
+        "accessToken" => token, 
+        "refreshToken" => refreshToken
+      }, 
+      "reconnectToVoice" => false,
+      "muted" => true,
+      "platform" => "dogehouse-cr"
+    }.to_json
+  )
 end
 
 ws.on_message do |msg|
   puts msg
 end
+
+ws.on_close do |code|
+  puts "Connection closed: #{code}"
+  exit 1
+end 
 
 ws.on_ping do
   puts "Received ping"
@@ -39,24 +55,7 @@ spawn do
   end
 end
 
-auth = JSON.build do |json|
-  json.object do
-    json.field "op", "auth"
-    json.field "d", do
-      json.object do
-        json.field "accessToken", ENV["ACCESS_TOKEN"]
-        json.field "refreshToken", ENV["REFRESH_TOKEN"]
-        json.field "reconnectToVoice", false
-        json.field "currentRoomId", ENV["ROOM_ID"]
-        json.field "muted", true
-        json.field "platform", "dogehouse-cr"
-      end
-    end
-  end
-end
-
-ws.send auth
-
+auth ws, ENV["ACCESS_TOKEN"], ENV["REFRESH_TOKEN"]
 join_room ws, "c1089644-cc15-4204-8935-336b5a7fa83c"
 
 ws.run
